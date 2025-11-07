1cf2aed12f4eff1e68335eaf095b2db96556223d
guoxin-qiu <guoxin.qiu@outlook.com>
2025-11-07 18:43:28 +0800

    deploy

---
+++ assets/menu-data.js
@@ -0,0 +1,27 @@
+const wordBankMenuData = [
+  {
+    title: '专业知识',
+    folder: 'capacity',
+    items: [
+      { id: 'itil-00', label: 'ITIL4' },
+      { id: 'itil-01-guide', label: 'ITIL4 Guide' },
+      { id: 'operation_book1', label: '运维数字化转型' },
+    ],
+  },
+  {
+    title: '英语知识',
+    folder: 'english',
+    items: [
+      { id: 'prefix-prof', label: '词-前缀(专)' },
+      { id: 'suffix-prof', label: '词-后缀(专)' },
+      { id: 'stem-prof', label: '词-词根(专)' },
+      { id: 'prefix', label: '词-前缀(普)' },
+      { id: 'suffix', label: '词-后缀(普)' },
+      { id: 'stem', label: '词-词根(普)' },
+      { id: 'phrase-work', label: '短语-工作' },
+      { id: 'phrase-cosmetic', label: '短语-化妆品' },
+      { id: 'food', label: '日常-食物' },
+      { id: 'vegetables', label: '日常-蔬菜' },
+    ],
+  },
+];
